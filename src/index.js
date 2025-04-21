0e6453f94945fb74bdc23fe053e92d680b64f1b4
jezzrs <jezz.ramirezs@gmail.com>
2025-04-21 18:13:24 +0100

    update index.js
    Added the rest of variables with let

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ function changeTemperature(response) {
   let temperatureNumber = document.querySelector("#temperature-number");
   let temperature = Math.round(response.data.temperature.current);
   let cityName= document.querySelector("#city");
-  let dateElement = document.querySelector ("#date");
   let humidityNumber = document.querySelector ("#humidity");
   let windSpeedNumber = document.querySelector("#wind-speed");
+  let dateTimeElements= document.querySelector ("#date-time")
+  let date = new Date(response.date.time*1000);
+  let iconElement = document.querySelector ("#icon");
+  let weatherDetail = document.querySelector("#weather-description")
   
 }
