b0338f41a96e3f6757dd5c659edb8069e322446d
jezzrs <jezz.ramirezs@gmail.com>
2025-04-21 18:07:47 +0100

    Update index.js
    Adding the elements to create the update in temperature, city, humidty, etc.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 function changeTemperature(response) {
-  let temperatureNumber = document.querySelector("temperature-number");
+  let temperatureNumber = document.querySelector("#temperature-number");
   let temperature = Math.round(response.data.temperature.current);
+  let cityName= document.querySelector("#city");
+  let dateElement = document.querySelector ("#date");
+  let humidityNumber = document.querySelector ("#humidity");
+  let windSpeedNumber = document.querySelector("#wind-speed");
+  
 }
