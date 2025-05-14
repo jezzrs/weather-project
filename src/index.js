6cd5f2a9b47593e7bf0ff80302e1c4cc326a16ae
jezzrs <jezz.ramirezs@gmail.com>
2025-05-14 20:47:41 +0100

    Updated to do the formatting
    Several elements were updatd to achieve the correct style within the app. I had to include additional divs to separate containers, as well as modify the JS so the information provided reflects correctly (e.g. hours)

--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
-
-
-
 function changeTemperature(response) {
   let temperatureNumber = document.querySelector("#temperature-number");
   let temperature = Math.round(response.data.temperature.current);
-  let cityName= document.querySelector("#city");
-  let humidityNumber = document.querySelector ("#humidity");
+  let cityName = document.querySelector("#city");
+  let humidityNumber = document.querySelector("#humidity");
   let windSpeedNumber = document.querySelector("#wind-speed");
-  let dateTimeElements= document.querySelector ("#date-time");
-  let iconElement = document.querySelector ("#icon");
+  let dateTimeElements = document.querySelector("#date-time");
+  let iconElement = document.querySelector("#icon");
   let weatherDetail = document.querySelector("#weather-description");
-  let date=new Date(response.data.time*1000)
-
+  let date = new Date(response.data.time * 1000);
 
   cityName.innerHTML = response.data.city;
-  dateTimeElements.innerHTML = formatDate (date);
+  dateTimeElements.innerHTML = formatDate(date);
   weatherDetail.innerHTML = response.data.condition.description;
   humidityNumber.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedNumber.innerHTML= `${response.data.wind.speed}km/h`;
+  windSpeedNumber.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureNumber.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-
 }
 
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
-  let day = date.getDay();
-
-  if(minutes<10){minutes=`0${minutes}`;
-}
-
   let days = [
     "Sunday",
     "Monday",
@@ -38,24 +28,29 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
+  let day = days[date.getDay()];
 
-let formattedDate = days[day];
-return `${formattedDate} ${hours} ${minutes}`;}
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-function searchCity(city){
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
   let apiKey = "b21d503dafo2f9t8344f5f54960e518f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(changeTemperature);
 }
 
-function stopRefreshSubmit (event){
-  event.preventDefault ();
+function stopRefreshSubmit(event) {
+  event.preventDefault();
   let searchInput = document.querySelector("#search-weather-input");
 
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-weather-form");
-searchFormElement.addEventListener("submit",stopRefreshSubmit);
+searchFormElement.addEventListener("submit", stopRefreshSubmit);
